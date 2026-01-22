bd8ee5a98cc097eb03ce5600703d4c2b0c51c1c7
ferrylau <ferrylau23@gmail.com>
2026-01-22 21:39:38 +0800

    update

--- logger.js
+++ logger.js
@@ -16,18 +16,18 @@ if (typeof $response !== 'undefined') {
         // --- 1. 执行非转义字段正则注入 ---
         let modifiedBody = body
             .replace(/"gems":\s*\d+/g, '"gems":999999')
-            .replace(/"energy":\s*\d+/g, '"energy":100') // 修改体力为 100
-            .replace(/"subscriberLevel":\s*".*?"/g, '"subscriberLevel":"super"');
+            .replace(/"energy":\s*\d+/g, '"energy":999') // 修改体力为 100
+            .replace(/"subscriberLevel":\s*".*?"/g, '"subscriberLevel":"Super"');
 
         // --- 2. 处理 Batch 特有的转义嵌套字段 ---
         // 这一步是为了确保在复杂 JSON 字符串中也能改掉数值
-        if (body.includes('"responses"')) {
-            modifiedBody = modifiedBody
-                .replace(/\\"gems\\":\s*\d+/g, '\\"gems\\":999999')
-                .replace(/\\"energy\\":\s*\d+/g, '\\"energy\\":100'); // 转义体力修改
-        }
+        // if (body.includes('"responses"')) {
+        //     modifiedBody = modifiedBody
+        //         .replace(/\\"gems\\":\s*\d+/g, '\\"gems\\":999999')
+        //         .replace(/\\"energy\\":\s*\d+/g, '\\"energy\\":999'); // 转义体力修改
+        // }
 
-        console.log(`[DUOLIN_FINAL] ✅ 注入完成 (宝石:999999, 体力:100)`);
+        console.log(`[DUOLIN_FINAL] ✅ 注入完成 (宝石:999999, 体力:999)`);
         $done({ body: modifiedBody });
     }
 } else {
